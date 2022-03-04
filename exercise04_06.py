dc1dfcbeafda73a34b0c0a73c5aa0714818e8384
Your Name <peyton@spreacker.com>
2022-03-03 21:11:04 -0600

    inital commit

---
+++ exercise04_06.py
@@ -0,0 +1,15 @@
+def computepay(h, r) :
+    if h > 40:
+        reg = h * r
+        otp = (h-40.0) * (r * 0.5)
+    else :
+        print("Regular")
+        reg = h * r
+        otp = 0
+        p = reg + otp
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate:")
+r = float (rate)
+p = computepay(h, r)
+print("Pay:", p)
